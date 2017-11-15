ec064f1add4a16d706c795d830e2f130121a8ca7
nappleyjq <napple.yjq@hotmail.com>
2017-11-15 20:28:13 +0800

    Initial commit

---
+++ config_submit.py
@@ -0,0 +1,13 @@
+config = {'datapath': '/media/akiko/AA5E12FF5E12C3CB/Kaggle/DSB2017/kaggle/sample_images',
+          'preprocess_result_path': 'prep_result/',
+          'outputfile': 'prediction.csv',
+
+          'detector_model': 'net_detector',
+          'detector_param': 'model/detector.ckpt',
+          'classifier_model': 'net_classifier',
+          'classifier_param': 'model/classifier.ckpt',
+          'n_gpu': 1,
+          'n_worker_preprocessing': None,
+          'use_exsiting_preprocessing': False,
+          'skip_preprocessing': False,
+          'skip_detect': False}
---
+++ preprocessing/__init__.py
@@ -0,0 +1,1 @@
+from preprocessing.full_prep import full_prep, savenpy
